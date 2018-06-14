96df0eefe569cd80ed51c263327eebe25c1bc476
coder <nishitkshah444@gmail.com>
2018-06-14 10:19:39 +0530

    Temporarily cleared all project files

--- app_node_proj/vm_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class VmAppConfig(AppConfig):
-    name = 'vm_app'
--- manager_node_proj/report_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ReportAppConfig(AppConfig):
-    name = 'report_app'
